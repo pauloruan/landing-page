7f6218f09faacd8e197e50aa14d003884fc11db6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-12-25 12:46:32 -0300

    refactor: Set to default theme font families and sizes

--- src/styles/themes/default.js
+++ src/styles/themes/default.js
@@ -4,6 +4,20 @@ export const defaultTheme = {
     secondary: '#dc143c',
     white: '#fff',
   },
-  fonts: {},
+  fonts: {
+    family: {
+      openSans: "'Open Sans', sans-serif",
+      montserrat: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+    },
+  },
   spacings: {},
 };
