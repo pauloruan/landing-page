4124cf7faaafad8634a8872c333ffc553e10ac0e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-12-25 18:05:49 -0300

    test: `Home` page test created

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyles';
 import { defaultTheme } from './styles/themes/default';
-import Home from './templates/App';
+import Home from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,8 @@
+import Home from '.';
+import { renderTheme } from '../../styles/renderTheme';
+
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
+  });
+});
