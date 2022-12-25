cbb9551b7043a044033cf127174795bd6da98280
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-12-25 13:09:05 -0300

    feat: Component `Heading` created and styled

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import * as Styled from './styles';
 
-export default function Heading({ children, dark = false }) {
-  return <Styled.Title dark={dark}>{children}</Styled.Title>;
+export default function Heading({ children, dark = false, as = 'h1', size = 'huge', uppercase = false }) {
+  return (
+    <Styled.Title dark={dark} as={as} size={size} uppercase={uppercase}>
+      {children}
+    </Styled.Title>
+  );
 }
 
 Heading.propTypes = {
   children: PropTypes.node.isRequired,
-  dark: PropTypes.bool.isRequired,
+  dark: PropTypes.bool,
+  as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge', 'huge']),
+  uppercase: PropTypes.bool,
 };
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: ${({ theme, size }) => theme.fonts.sizes[size]};
   text-align: center;
   color: ${({ theme, dark }) => (dark ? theme.colors.white : theme.colors.secondary)};
+  text-transform: ${({ uppercase }) => (uppercase ? 'uppercase' : 'none')};
+
+  @media (max-width: 768px) {
+    font-size: ${({ theme }) => theme.fonts.sizes.xlarge};
+  }
 `;
